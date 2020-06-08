2f4df4c1538da23772a2e535a2013e712bb4652f
Chelseaquiin <60669974+Chelseaquiin@users.noreply.github.com>
2020-06-08 12:28:22 +0100

    Update HNG-02055.java
    I effected a change to my work.

--- scripts/HNG-02055.java
+++ scripts/HNG-02055.java
@@ -1,4 +1,4 @@
-class HNG{
+public class HNG{
     public static void main(string [] args){
         System.out.println("Hello World, this is Uzoamaka Nweze with HNGi7 ID [HNG-02055] using java for stage 2 task. uzonweze@gmail.com");
     }
